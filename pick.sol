23ca10228bd34cf23a6c2ff6a3e9883f6cd8fd9a
Phil P <philip.pun1@gmail.com>
2021-10-07 22:40:48 +0800

    High level function code

---
+++ pick.sol
@@ -0,0 +1,37 @@
+
+function registerPlayer(address _voter) {
+
+	// registers voter
+
+};
+
+
+function hostToRollDice(uint diceNumber) {
+
+// roll the Dice
+};
+
+
+
+function placingBet(uint diceNumber) {
+
+// record player bet and 
+};
+
+
+function pickTheClosest(uint userID) {
+
+// pick the closest winner 
+};
+
+
+function removeDuplicateBet(uint diceNumber) {
+
+// when there are duplicated bet on the same number, do something
+};
+
+function distributeMoney(uint diceNumber) payable {
+
+// send the collected money to the winner
+};
+
